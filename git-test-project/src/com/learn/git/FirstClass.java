cb43c12f95103cc08efbeff5f507b73d242b8e67
zms <2253768856@qq.com>
2020-10-21 10:33:07 +0800

    提交git-test-project

---
+++ git-test-project/src/com/learn/git/FirstClass.java
@@ -0,0 +1,13 @@
+package com.learn.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("我是git");
+        System.out.println("我是git");
+        System.out.println("我是git");
+    }
+    public void test(){
+        int i=0;
+        i+=10;
+    }
+}
